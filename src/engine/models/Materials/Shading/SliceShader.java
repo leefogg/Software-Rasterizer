be48906bc2be7f7668fc7dbe25b4f56aed46aad5
Lee Fogg <leefogg@gmail.com>
2016-06-22 21:12:05 +0100

    Added optional offsets to StripShader and SliceShader

--- src/engine/models/Materials/Shading/SliceShader.java
+++ src/engine/models/Materials/Shading/SliceShader.java
@@ -1,14 +1,19 @@
 package engine.models.Materials.Shading;
 
 public class SliceShader extends Shader {
-	public float height;
+	public float height, offset;
 
 	public SliceShader(float height) {
+		this(height, 0);
+	}
+	public SliceShader(float height, float offset) {
 		this.height = height;
+		this.offset = offset;
 	}
 
 	@Override
 	public void shade() {
+		worldPosition.y += offset;
 		worldPosition.y %= height;
 		if (Math.abs(worldPosition.y) < height / 2) {
 			getTextureColor();
--- src/engine/models/Materials/Shading/StripShader.java
+++ src/engine/models/Materials/Shading/StripShader.java
@@ -3,17 +3,21 @@ package engine.models.Materials.Shading;
 import engine.math.Color;
 
 public class StripShader extends Shader {
-	public float height;
+	public float height, offset;
 	public Color color1, color2;
 
 	public StripShader(float height, Color color1, Color color2) {
+		this(height, 0, color1, color2);
+	}
+	public StripShader(float height, float offset, Color color1, Color color2) {
 		this.height = height;
 		this.color1 = color1;
-		this.color2 = color2;
+		this.color2 = color2;		
 	}
 
 	@Override
 	public void shade() {
+		worldPosition.y += offset;
 		worldPosition.y %= height;
 		if (Math.abs(worldPosition.y) < height / 2) {
 			destinationColor.set(color1);
